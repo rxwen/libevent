56d31ed61b228aa7aefdb636097e01115c49e112
rxwen <rx.wen218@gmail.com>
2017-04-07 22:11:10 +0800

    support old android.mk format, and add http-server

---
+++ Android.mk
@@ -0,0 +1,62 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+        buffer.c \
+        bufferevent.c \
+        bufferevent_filter.c \
+        bufferevent_pair.c \
+        bufferevent_ratelim.c \
+        bufferevent_sock.c \
+        event.c \
+        evmap.c \
+        evthread.c \
+        evutil.c \
+        evutil_rand.c \
+        listener.c \
+        log.c \
+        signal.c \
+        strlcpy.c \
+        evdns.c \
+        event_tagging.c \
+        evrpc.c \
+        http.c \
+        poll.c \
+        epoll.c \
+        select.c
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include
+
+LOCAL_CFLAGS += -D_BSD_SOURCE \
+				 -Wno-implicit-function-declaration \
+				 -Wno-strict-aliasing \
+				 -DSIZE_MAX=4294967295U
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_MODULE := libevent
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+        http-server.c
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include
+
+LOCAL_CFLAGS += -D_BSD_SOURCE \
+				 -Wno-implicit-function-declaration \
+				 -Wno-strict-aliasing \
+				 -DSIZE_MAX=4294967295U
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_SHARED_LIBRARIES += libevent
+
+LOCAL_MODULE := httpserver
+
+include $(BUILD_EXECUTABLE)
